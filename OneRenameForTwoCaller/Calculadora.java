0511137ee653a2e1b1127853543aebe5f7f1590f
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-26 22:21:35 -0300

    code after refact rename method

--- OneRenameForTwoCaller/Calculadora.java
+++ OneRenameForTwoCaller/Calculadora.java
@@ -3,8 +3,8 @@ public class Calculadora {
     int resultado = 0;
 
     if (operacao.equals("soma")) {
-      resultado = soma(num1, num2);
-      resultado = soma(num1, num2);
+      resultado = sum(num1, num2);
+      resultado = sum(num1, num2);
     } else if (operacao.equals("subtracao")) {
       resultado = subtracao(num1, num2);
     } else if (operacao.equals("multiplicacao")) {
@@ -18,7 +18,7 @@ public class Calculadora {
     return resultado;
   }
 
-  private int soma(int num1, int num2) {
+  private int sum(int num1, int num2) {
     return num1 + num2;
   }
 
