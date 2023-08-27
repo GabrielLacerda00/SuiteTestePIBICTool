469bda33f798aae941a6fafc44d02b774c3d6b54
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-26 21:49:08 -0300

    no rename method

---
+++ NoRenameMethod/src/codeDestiny/calculadora.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Method;
+
+public class calculadora extends calc{
+  public static void main(String[] args) {
+   
+  }
+
+  public static double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
