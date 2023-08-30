89d963488d08b3817358ad10fae0cf9baab6a06e
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-30 10:09:22 -0300

    code before refactoring: rename method

---
+++ OneRenameForOneObjCallerInOtherClasse/src/CodeOrigin/calculadora.java
@@ -0,0 +1,16 @@
+package CodeOrigin;
+
+import java.lang.reflect.Method;
+import CodeOrigin.calculadoraCientifica.calc;
+
+public class calculadora extends calc{
+  public static void main(String[] args) {
+    calc cal = new calc();
+    cal.soma();
+  }
+
+  public double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
