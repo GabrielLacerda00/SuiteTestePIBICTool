74bc655524a92221424b5f4ff42d33423940c7a3
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-31 14:54:20 -0300

    code before refactoring: rename method

---
+++ OneRenameForMissingObjCallersInOtherClasses/calcul.java
@@ -0,0 +1,17 @@
+
+
+import java.lang.reflect.Method;
+import CodeDestiny.calculadoraCientifica.calc;
+
+public class calcul extends calc{
+    public static void main(String[] args) {
+    calc cal = new calc();
+     cal.soma();
+     cal.soma();
+  }
+
+  public static double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
---
+++ OneRenameForMissingObjCallersInOtherClasses/calculadoraCientifica/calc.java
@@ -0,0 +1,13 @@
+package calculadoraCientifica;
+
+public class calc {
+  public static void main(String[] args) {
+    calc cal = new calc();
+    cal.soma();
+    cal.soma();
+  }
+  
+  public double sum() {
+      return 1.0 + 2.0;
+  }
+}
