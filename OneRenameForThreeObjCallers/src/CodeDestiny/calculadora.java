6eab6d4fc64d286b00787c4e5c613542b6bed80b
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-31 20:58:29 -0300

    code before refactoring: rename method

---
+++ OneRenameForThreeObjCallers/src/CodeDestiny/calculadora.java
@@ -0,0 +1,16 @@
+package CodeDestiny;
+
+import java.lang.reflect.Method;
+import CodeDestiny.calculadoraCientifica.calc;
+
+public class calculadora extends calc{
+  public static void main(String[] args) {
+    calc cal = new calc();
+    cal.sum();
+  }
+
+  public double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
