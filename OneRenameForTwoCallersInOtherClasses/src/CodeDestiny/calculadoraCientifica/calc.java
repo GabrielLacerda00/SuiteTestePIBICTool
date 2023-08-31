00cf75ca4908ede2d6d704c9bcc72323beca2cde
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-08-30 22:10:49 -0300

    code before reafactoring: rename method

---
+++ OneRenameForTwoCallersInOtherClasses/src/CodeDestiny/calculadoraCientifica/calc.java
@@ -0,0 +1,11 @@
+package CodeDestiny;
+
+public class calc {
+  public static void main(String[] args) {
+   sum();
+  }
+  
+  public double sum() {
+      return 1.0 + 2.0;
+  }
+}
---
+++ OneRenameForTwoCallersInOtherClasses/src/calcul.java
@@ -0,0 +1,15 @@
+
+
+import java.lang.reflect.Method;
+import calculadoraCientifica;
+
+public class calcul extends calc{
+  public static void main(String[] args) {
+    soma();
+  }
+
+  public double soma() {
+    return 1.0 + 2.0;
+  }
+
+}
